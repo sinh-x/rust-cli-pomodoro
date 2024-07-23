5d1c071b0b0fd9cd9eac7f73dcdb4a02a7a536f9
sinh-x <sinh@sinhn.com>
2024-07-23 07:21:53 +0700

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,33 @@
+{
+  description = "Rust cli pomodoro";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        defaultPackage = pkgs.rustPlatform.buildRustPackage rec {
+          pname = "rust_cli_pomodoro";
+          version = "1.4.5";
+          src = ./.;
+          cargoSha256 = "";
+          buildInputs = [pkgs.openssl];
+          nativeBuildInputs = [pkgs.cargo pkgs.rustc pkgs.pkg-config pkgs.openssl];
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [pkgs.openssl];
+        };
+
+        devShell = pkgs.mkShell {
+          buildInputs = [pkgs.openssl];
+          nativeBuildInputs = [pkgs.cargo pkgs.rustc pkgs.pkg-config pkgs.openssl];
+          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [pkgs.openssl];
+        };
+      }
+    );
+}
